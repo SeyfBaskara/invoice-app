80f0b073ae78b360adc97772afc763f1477ed11c
Seyf <seyfettin.baskara@gmail.com>
2023-03-23 21:35:20 +0100

    add css on header

--- src/components/Layout/Header/Header.vue
+++ src/components/Layout/Header/Header.vue
@@ -16,16 +16,43 @@ export default {
 
 <template>
   <header class="header">
-    <Logo />
-    <div>
+    <div class="logo">
+      <Logo />
+    </div>
+    <div class="header-right">
       <IconMoon />
-      <img :src="avatarImage" alt="avatar image" />
+      <img class="avatar" :src="avatarImage" alt="avatar image" />
     </div>
   </header>
 </template>
 
 <style scoped>
 .header {
-  background-color: purple;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 70px;
+  background-color: var(--bg-secondary-color);
+}
+.logo {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: var(--primary-color);
+  height: 100%;
+  width: 80px;
+  border-top-right-radius: 30%;
+  border-bottom-right-radius: 30%;
+}
+.header-right {
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  width: 160px;
+}
+.avatar {
+  height: 35px;
+  width: 35px;
+  border-radius: 50%;
 }
 </style>
