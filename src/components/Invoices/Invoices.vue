7c0622aa26ccd0a71806546ffe0ef73c92a91cb2
Seyf <seyfettin.baskara@gmail.com>
2023-03-28 17:01:59 +0200

    add basic css on invoice item

--- src/components/Invoices/Invoices.vue
+++ src/components/Invoices/Invoices.vue
@@ -20,23 +20,79 @@ export default {
 </script>
 
 <template>
-  <div>
-    <ul>
-      <li v-for="invoice in invoices" :key="invoice.id">
-        <div>
-          <p>{{ invoice.id }}</p>
-          <p>{{ invoice.clientName }}</p>
+  <div class="invoices">
+    <ul class="invoices__list">
+      <li v-for="invoice in invoices" :key="invoice.id" class="invoice__item">
+        <div class="invoice__header">
+          <p class="header__id">#{{ invoice.id }}</p>
+          <p class="header__name">{{ invoice.clientName }}</p>
         </div>
-        <div>
+        <div class="invoice__body">
           <div>
-            <p>{{ invoice.paymentDue }}</p>
+            <p>Due {{ invoice.paymentDue }}</p>
             <p>{{ invoice.total }}</p>
           </div>
-          <p>{{ invoice.status }}</p>
+          <p class="body__status">{{ invoice.status }}</p>
         </div>
       </li>
     </ul>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.invoices {
+  margin-top: 2rem;
+}
+.invoices__list {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+}
+.invoice__item {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  background-color: white;
+  border-radius: 10px;
+  padding: 1.2rem;
+}
+.invoice__header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.header__id {
+  font-size: small;
+  font-weight: bold;
+}
+.header__name {
+  font-size: small;
+  color: var(--text-primary-color);
+}
+.invoice__body {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.body__status {
+  background-color: lightpink;
+  width: 100px;
+  text-align: center;
+  border-radius: 10px;
+  font-weight: bold;
+  font-size: small;
+  padding: 0.5rem;
+}
+/* .body__status::before {
+  content: "\2022";
+  display: inline-block;
+  width: 1rem;
+  position: relative;
+  top: 7px;
+  right: 10px;
+  font-size: xx-large;
+} */
+</style>
