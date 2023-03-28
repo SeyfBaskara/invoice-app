eb902455b2fb6a3b3cd10c17a2432b2d6582b8d7
Seyf <seyfettin.baskara@gmail.com>
2023-03-28 16:04:52 +0200

    fetch data and render basic info

--- src/components/Invoices/Invoices.vue
+++ src/components/Invoices/Invoices.vue
@@ -1,17 +1,42 @@
 <script>
 export default {
   components: {},
+  data: function () {
+    return {
+      invoices: [],
+    };
+  },
+  methods: {
+    async fetchInvoices() {
+      const res = await fetch("http://localhost:5000/invoices");
+      const data = await res.json();
+      return data;
+    },
+  },
+  async created() {
+    this.invoices = await this.fetchInvoices();
+  },
 };
 </script>
 
 <template>
   <div>
-    <p>Invoice app</p>
+    <ul>
+      <li v-for="invoice in invoices" :key="invoice.id">
+        <div>
+          <p>{{ invoice.id }}</p>
+          <p>{{ invoice.clientName }}</p>
+        </div>
+        <div>
+          <div>
+            <p>{{ invoice.paymentDue }}</p>
+            <p>{{ invoice.total }}</p>
+          </div>
+          <p>{{ invoice.status }}</p>
+        </div>
+      </li>
+    </ul>
   </div>
 </template>
 
-<style scoped>
-p {
-  color: blue;
-}
-</style>
+<style scoped></style>
