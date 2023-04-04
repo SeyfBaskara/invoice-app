4cd525a63ac30ed615ad1076bbaec9adab0616be
Seyf <seyfettin.baskara@gmail.com>
2023-04-04 16:32:15 +0200

    add css on control button

--- src/components/InvoiceDetails/StatusWidget.vue
+++ src/components/InvoiceDetails/StatusWidget.vue
@@ -18,9 +18,11 @@ export default {
       <p :class="`invoice__status ${status}`">{{ status }}</p>
     </div>
     <div class="widget__buttons">
-      <button>Edit</button>
-      <Button>Delete</Button>
-      <Button v-show="!isDraft">Mark as {{ setStatus }}</Button>
+      <button class="button edit">Edit</button>
+      <Button class="button delete">Delete</Button>
+      <Button class="button mark-as" v-show="!isDraft"
+        >Mark as {{ setStatus }}</Button
+      >
     </div>
   </section>
 </template>
@@ -38,13 +40,6 @@ export default {
 .widget__status > h2 {
   font-size: 1.2rem;
   color: var(--text-primary-color);
-}
-.widget__buttons {
-  position: fixed;
-  bottom: 0;
-  left: 0;
-  width: 100%;
-  border-radius: 0;
 }
 .invoice__status {
   width: 100px;
@@ -74,5 +69,32 @@ export default {
   top: 3px;
   margin-right: 0.3rem;
   font-size: x-large;
+}
+
+.widget__buttons {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  border-radius: 0;
+}
+.button {
+  padding: 0.7rem 1.2rem;
+  border: none;
+  border-radius: 20px;
+  cursor: pointer;
+  font-weight: 600;
+}
+.edit {
+  background-color: var(--bg-light-color);
+  color: var(--text-secondary-color);
+}
+.delete {
+  background-color: var(--delete-color);
+  color: white;
+}
+.mark-as {
+  background-color: var(--primary-color);
+  color: white;
 }
 </style>
