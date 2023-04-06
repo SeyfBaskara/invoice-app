25f46650dde4ba7421504e90a4bd68aa6db12a12
Seyf <seyfettin.baskara@gmail.com>
2023-04-06 18:37:59 +0200

    change currency locale code eu to gbp

--- src/utils/formatCurrency.js
+++ src/utils/formatCurrency.js
@@ -1,5 +1,5 @@
-const CURRENCY_FORMATTER = new Intl.NumberFormat("de-DE", {
-  currency: "EUR",
+const CURRENCY_FORMATTER = new Intl.NumberFormat("en-GB", {
+  currency: "GBP",
   style: "currency",
 });
 
