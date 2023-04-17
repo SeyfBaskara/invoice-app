d94ceae93dc1677c7d51efcb6e46fb3fdd2d8f09
Seyf <seyfettin.baskara@gmail.com>
2023-04-17 17:15:59 +0200

    fix the url fetch for the status

--- src/api/index.js
+++ src/api/index.js
@@ -3,5 +3,7 @@ import axios from "axios";
 const url = "http://localhost:5000";
 
 export const fetchInvoices = async () => await axios.get(`${url}/invoices`);
+export const fetchInvoicesByStatus = async (status) =>
+  await axios.get(`${url}/invoices?status=${status}`);
 export const deleteInvoice = async (id) =>
   await axios.delete(`${url}/invoices/${id}`);
