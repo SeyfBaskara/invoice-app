62b6fd4f981217a804af1a7debd233c420a9d651
Seyf <seyfettin.baskara@gmail.com>
2023-08-13 14:56:16 +0200

    add config for runnning serverless function on vercel

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const url = "http://localhost:5000";
+// const url = "http://localhost:5000";
+const url = "https://invoice-app-opal.vercel.app/api/data";
 
 export const fetchInvoices = async () => await axios.get(`${url}/invoices`);
 export const fetchInvoicesByStatus = async (status) =>
